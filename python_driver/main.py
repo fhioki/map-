8de6ab3045ae709c9ca4797beada10c05ffc3b46
marco masciola <marco.masciola@gmail.com>
2014-12-26 09:31:35 -0600

    tested merged commits with baseline tests

--- python_driver/main.py
+++ python_driver/main.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     # mooring_1.read_file("../test/baseline_4.map") # 100 m depth
     # mooring_1.read_file("../test/baseline_5.map") # 80 m depth
     # mooring_1.read_file("../test/OMAERB_Mooring.dat") # 100 m depth
-    mooring_1.read_file("../test/NRELOffshrBsline5MW_Platform_OC3Hywind.map") # 320 m depth
+    # mooring_1.read_file("../test/NRELOffshrBsline5MW_Platform_OC3Hywind.map") # 320 m depth
     # mooring_1.read_file("../test/NRELOffshrBsline5MW_Platform_OC3Hywind_segmented.map") # 320 m depth
     # mooring_1.read_file("../test/NRELOffshrBsline5MW_Platform_OC3Hywind.map") # 320 m depth
     # mooring_1.read_file("../test/NRELOffshrBsLine5MW_TLP.map") # 200 m depth
